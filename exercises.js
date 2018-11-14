34fd224bbe31bd418cc41c21a2515c0bc29dc2e5
Roland Manangan <roland.manangan@gmail.com>
2018-11-13 19:11:33 -1000

    Step 9 complete

--- exercises.js
+++ exercises.js
@@ -205,8 +205,15 @@ console.log(spareChange(99)); // false
  * The function will return true if the sum of all the number values is greater than 30, otherwise it will return false.
  * Console.log your result.
 */ 
-
-
+function dirty30(one,two,three){
+  if (one+two+three > 30){
+    return true;
+  }else{
+    return false;
+  }
+}
+console.log(dirty30(10,11,12)); //true
+console.log(dirty30(7,8,9)); //false
 
 /*
  * #10
